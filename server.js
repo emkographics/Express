602d1a9ed142e09efaa1edf0207a6e508b3d92c4
Memic <emir.memic@mercy.net>
2016-07-10 19:37:30 -0500

    Code reformat

--- server.js
+++ server.js
@@ -1,42 +1,18 @@
 var express = require('express');
 var app = express();
 var PORT = 3001;
-var date = new Date().toString();
 
-/*
-app.get('/', function(req, res){
-	res.send("Hello Express");
-});
-*/
-
-var middleware = {
-	requireAuthentication: function(re1, res, next) {
-		console.log('private route hit!');
-		next();
-  },
-  logger: function(req, res, next) {
-	  console.log('Request: [' + date + '] ---> '  + req.method + ' ' + req.originalUrl );
-	  next();
-  }
-}
-
-//app.use(middleware.requireAuthentication); // applys middleware on every requrest.
+var middleware = require('./middleware.js');
 app.use(middleware.logger); // logs all url requests.
-/*
-//applys middlware to aboutus page only
+
 app.get('/about-us', middleware.requireAuthentication, function(req, res){
 	res.send("About Us");
 });
-*/
 
-app.get('/about-us', function(req, res){
-	res.send("About Us");
-});
-
-app.use(express.static(__dirname + '/public'));
-
-//console.log(__dirname);
+app.use(express.static(__dirname + '/public')); // define root path
 
 app.listen(PORT, function() {
 	console.log("Express server started on port: " + PORT);
 });
+
+//console.log(__dirname); // check the directory path
